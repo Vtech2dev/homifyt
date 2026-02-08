2ee46a8934a5bbfcd37260c107e00dcfc3eba530
Vtech2dev <jaffarasad3120@gmail.com>
2026-02-08 18:35:38 +0300

    Delete ap1 directory

--- ap1/response.php
+++
@@ -1,4 +0,0 @@
-<?php
-function ok($data=null) { echo json_encode(["ok"=>true, "data"=>$data]); exit; }
-function err($msg, $code=400) { http_response_code($code); echo json_encode(["ok"=>false, "error"=>$msg]); exit; }
-function read_json() { $raw=file_get_contents("php://input"); $in=json_decode($raw,true); return is_array($in)?$in:[]; }
